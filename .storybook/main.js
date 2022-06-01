16280a8c703411a2db8db7a75d19235637d7edd2
Aaron Sandusky <atsand23@gmail.com>
2022-06-01 14:10:45 -0600

    add new test component. add new story and specs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.js'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   features: {
     postcss: false,
   },
-  framework: "@storybook/vue3",
+  framework: '@storybook/vue3',
   core: {
-    builder: "webpack4",
+    builder: 'webpack4',
   },
 };
